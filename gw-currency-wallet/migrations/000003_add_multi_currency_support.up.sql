a55f783e1d7458eeb89de67243609ae74a2fe48f
ayrinwave2002 <122171532+ayrinwave@users.noreply.github.com>
2025-11-24 18:34:45 +0300

    bug fix

--- gw-currency-wallet/migrations/000003_add_multi_currency_support.up.sql
+++ gw-currency-wallet/migrations/000003_add_multi_currency_support.up.sql
@@ -1,3 +1,11 @@
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+
 -- Таблица пользователей
 CREATE TABLE IF NOT EXISTS users (
                                      id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
